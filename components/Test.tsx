79b6878a071d87c74063181fcac62c56f9a60b72
Sean OReilly <soreilly424@gmail.com>
2023-01-04 14:42:31 -0500

    adding container

--- components/Test.tsx
+++ components/Test.tsx
@@ -5,11 +5,14 @@ const atom1 = atom(false)
 export const Test = () => {
   const [isTrue, setIsTrue] = useAtom(atom1)
   return (
-    <div>
-      <button onClick={() => setIsTrue((prevState) => !prevState)}>
+    <div className='flex gap-3 items-center'>
+      <button
+        className='button'
+        onClick={() => setIsTrue((prevState) => !prevState)}
+      >
         Toggle Local State!
       </button>
-      <p>{isTrue ? 'true' : 'false'}</p>
+      <p>state: {isTrue ? 'true' : 'false'}</p>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    container: {
+      padding: '2rem',
+      center: true,
+    },
     extend: {},
   },
   plugins: [],
