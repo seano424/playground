7415560200ce0d2956e2e12879f7773ef55bd69b
Sean OReilly <soreilly424@gmail.com>
2023-01-05 09:40:28 -0500

    tried adding prettier

--- components/Test.tsx
+++ components/Test.tsx
@@ -5,9 +5,9 @@ const atom1 = atom(false)
 export const Test = () => {
   const [isTrue, setIsTrue] = useAtom(atom1)
   return (
-    <div className='flex gap-3 items-center'>
+    <div className="flex gap-3 items-center">
       <button
-        className='button'
+        className="button"
         onClick={() => setIsTrue((prevState) => !prevState)}
       >
         Toggle Local State!
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+  htmlWhitespaceSensitivity: 'ignore',
+  tailwindConfig: './tailwind.config.js',
+}
